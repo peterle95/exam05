c9eeef09f373c46a0ea16e4459de5f3de99c9185
Peter Moelzer <moelzerpeter@gmail.com>
2025-05-19 16:28:32 +0200

    norm

---
+++ lvl_00/polyset/bag.hpp
@@ -0,0 +1,16 @@
+// FILE GIVEN DURING EXAM
+
+#ifndef EXAM_ASSIGNMENTS_BAG_HPP
+#define EXAM_ASSIGNMENTS_BAG_HPP
+
+class bag
+{
+public:
+    virtual void insert(int) = 0;
+    virtual void insert(int *, int) = 0;
+    virtual void print() const = 0;
+    virtual void clear() = 0;
+};
+
+#endif
+
---
+++ lvl_00/polyset/searchable_bag.hpp
@@ -0,0 +1,13 @@
+// FILE GIVEN DURING EXAM
+
+#ifndef EXAM_ASSIGNMENTS_SORTABLE_HPP
+#define EXAM_ASSIGNMENTS_SORTABLE_HPP
+#include "bag.hpp"
+
+class searchable_bag : virtual public bag {
+    public:
+        virtual bool has(int) const = 0;
+};
+
+#endif // EXAM_ASSIGNMENTS_SORTABLE_HPP
+
